0ec6df058519461e74a6cd8a39d76064eac256c4
Gibran <mac01gibran@gmail.com>
2022-04-03 15:13:17 -0300

    test: add Category entity

---
+++ src/@seedwork/domain/entity/entity.spec.ts
@@ -0,0 +1,30 @@
+import { validate as uuidValidate } from 'uuid';
+
+import { Entity } from './entity';
+import { UniqueEntityId } from './../value-objects/unique-entity-id.vo';
+
+
+class EntityStub extends Entity<{ prop1: string }> {
+  constructor(prop1: string, id?: UniqueEntityId) { super({ prop1 }, id); }
+}
+
+describe('Entity Unity Tests', () => {
+  test('Entity constructor', () => {
+    let sut = new EntityStub('value1')
+    expect(sut).toBeTruthy();
+    expect(sut).toBeInstanceOf(Entity);
+    expect(sut.props.prop1).toBe('value1');
+    expect(sut.id).toBeTruthy();
+    expect(sut.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
+    expect(uuidValidate(sut.id)).toBe(true);
+
+    const uniqueId = new UniqueEntityId()
+    sut = new EntityStub('value1', uniqueId);
+    expect(sut.id).toBe(uniqueId.value);
+    expect(sut.uniqueEntityId).toBeInstanceOf(UniqueEntityId);
+    expect(sut.toJSON()).toStrictEqual({
+      prop1: 'value1',
+      id: uniqueId.value
+    });
+  });
+})
---
+++ src/category/domain/entities/category.spec.ts
@@ -0,0 +1,85 @@
+import { Category, CategoryProperties } from './category'
+
+
+describe('Category Unit Tests', () => {
+  test('Category constructor', () => {
+    type TestProps = { input: CategoryProperties, expected: any }
+    const validateSpy = jest.spyOn(Category.prototype as any, 'validate')
+    const createdAt = new Date()
+    const testsProps: TestProps[] = [
+      { input: { name: 'Movie' }, expected: { name: 'Movie', description: null, isActive: false, createdAt: expect.any(Date) } },
+      { input: { name: 'Movie', description: 'A movie category' }, expected: { name: 'Movie', description: 'A movie category', isActive: false, createdAt: expect.any(Date) } },
+      { input: { name: 'Movie', description: 'A movie category', isActive: true }, expected: { name: 'Movie', description: 'A movie category', isActive: true, createdAt: expect.any(Date) } },
+      { input: { name: 'Movie', description: 'A movie category', isActive: true, createdAt }, expected: { name: 'Movie', description: 'A movie category', isActive: true, createdAt } },
+    ]
+
+    testsProps.forEach(({ input, expected }) => {
+      const category = new Category(input)
+      expect(category).toBeInstanceOf(Category)
+      expect(category.props).toEqual(expected)
+      expect(validateSpy).toHaveBeenCalled()
+    })
+  })
+
+  it ('should throw an error if name is null', () => {
+    expect(() => new Category({ name: null })).toThrowError('Name is required')
+  })
+
+  test('getter of name field', () => {
+    const sut = new Category({ name: 'Movie' })
+    expect(sut.name).toBe('Movie')
+  })
+
+  test('getter and setter of description field', () => {
+    let sut = new Category({ name: 'Movie' })
+    expect(sut.description).toBeNull()
+
+    sut = new Category({ name: 'Movie', description: 'A movie category' })
+    expect(sut.description).toBe('A movie category')
+  })
+
+  test('getter and setter of isActive field', () => {
+    let sut = new Category({ name: 'Movie' })
+    expect(sut.isActive).toBe(false)
+
+    sut = new Category({ name: 'Movie', isActive: true })
+    expect(sut.isActive).toBe(true)
+  })
+
+  test('getter and setter of createdAt field', () => {
+    let sut = new Category({ name: 'Movie' })
+    expect(sut.createdAt).toBeInstanceOf(Date)
+
+    const createdAt = new Date()
+    sut = new Category({ name: 'Movie', isActive: true, createdAt })
+    expect(sut.createdAt).toBe(createdAt)
+  })
+
+  test('update method', () => {
+    const createdAt = new Date()
+    const sut = new Category({ name: 'Movie', description: 'A movie category', createdAt })
+    expect(sut.name).toBe('Movie')
+    expect(sut.description).toBe('A movie category')
+
+    sut.update({ name: 'New Movie', description: 'A new movie category' })
+    expect(sut.props).toEqual({ name: 'New Movie', description: 'A new movie category', isActive: false, createdAt })
+    expect(sut.name).toBe('New Movie')
+    expect(sut.description).toBe('A new movie category')
+  })
+
+  test('activate method', () => {
+    const sut = new Category({ name: 'Movie', isActive: false })
+    expect(sut.isActive).toBe(false)
+
+    sut.activate()
+    expect(sut.isActive).toBe(true)
+  })
+
+  test('deactivate method', () => {
+    const sut = new Category({ name: 'Movie', isActive: true })
+    expect(sut.isActive).toBe(true)
+
+    sut.deactivate()
+    expect(sut.isActive).toBe(false)
+  })
+})
