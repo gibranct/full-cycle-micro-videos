9af5c5737d939e332077670470d1c32b8d56a9ed
Gibran <mac01gibran@gmail.com>
2022-04-21 22:34:39 -0300

    feat: add InMemoryRepository implementation

---
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -0,0 +1,109 @@
+import { NotFoundError } from './../errors/not-found.error'
+import {
+  RepositoryInterface,
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from './repository-contracts'
+import { Entity } from '../entity/entity'
+import { UniqueEntityId } from './../value-objects/unique-entity-id.vo'
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = []
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity)
+  }
+
+  async update(entity: E): Promise<void> {
+    const _id = entity.id
+    await this._get(_id)
+    const indexFound = this.items.findIndex((x) => x.id === _id)
+    this.items[indexFound] = entity
+  }
+
+  async delete(id: string | UniqueEntityId): Promise<void> {
+    const _id = `${id}`
+    await this._get(`${id}`)
+    this.items = this.items.filter((x) => x.id !== _id)
+  }
+
+  findById(id: string | UniqueEntityId): Promise<E> {
+    return this._get(`${id}`)
+  }
+
+  async findAll(): Promise<E[]> {
+    return [...this.items]
+  }
+
+  protected async _get(id: string): Promise<E> {
+    const item = this.items.find((x) => x.id === id)
+    if (!item) throw new NotFoundError('Entity not found')
+    return item
+  }
+}
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E>
+{
+  sortableFields: string[] = []
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const filteredItems = await this.applyFilter(this.items, props.filter)
+    const sortedItems = await this.applySort(
+      filteredItems,
+      props.sort,
+      props.sortDir
+    )
+    const paginatedItems = await this.applyPaginate(
+      sortedItems,
+      props.page,
+      props.perPage
+    )
+
+    return new SearchResult({
+      items: paginatedItems,
+      total: filteredItems.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    })
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: SearchParams['filter'] | null
+  ): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: SearchParams['sort'] | null,
+    sortDir: SearchParams['sortDir'] | null
+  ): Promise<E[]> {
+    if (!sort || !this.sortableFields.includes(sort)) return items
+    if (sortDir !== 'ASC' && sortDir !== 'DESC') return items
+    return [...items].sort((a: any, b: any) => {
+      if (sortDir === 'ASC') {
+        return a.props[sort] > b.props[sort] ? 1 : -1
+      } else {
+        return a.props[sort] > b.props[sort] ? -1 : 1
+      }
+    })
+  }
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage']
+  ): Promise<E[]> {
+    if (typeof page !== 'number' || typeof perPage !== 'number') return items
+    if (page <= 0 || perPage <= 0) return items
+    const start = (page - 1) * perPage
+    const end = page * perPage
+    return items.slice(start, end)
+  }
+}
---
+++ src/@seedwork/domain/repository/repository-contracts.ts
@@ -0,0 +1,155 @@
+import { UniqueEntityId } from './../value-objects/unique-entity-id.vo'
+import { Entity } from '../entity/entity'
+
+export interface RepositoryInterface<E extends Entity> {
+  insert(entity: E): Promise<void>
+  findById(id: string | UniqueEntityId): Promise<E>
+  findAll(): Promise<E[]>
+  update(entity: E): Promise<void>
+  delete(id: string | UniqueEntityId): Promise<void>
+}
+
+export type SortDirection = 'ASC' | 'DESC'
+
+export type SearchProps<Filter = string> = {
+  page?: number | null
+  perPage?: number | null
+  sort?: string | null
+  sortDir?: SortDirection | null
+  filter?: Filter | null
+}
+
+export class SearchParams<Filter = string> {
+  protected _page: number
+  protected _perPage: number = 15
+  protected _sort: string | null
+  protected _sortDir: SortDirection | null
+  protected _filter: Filter | null
+
+  constructor(props: SearchProps<Filter> = {}) {
+    this.page = props.page || 1
+    this.perPage = props.perPage || 15
+    this.sort = props.sort || null
+    this.sortDir = props.sortDir || null
+    this.filter = props.filter || null
+  }
+
+  get page(): number {
+    return this._page
+  }
+
+  private set page(value: number) {
+    let _page = +value
+    if (
+      Number.isNaN(_page) ||
+      _page <= 0 ||
+      Number.parseInt(`${_page}`) !== _page
+    ) {
+      _page = 1
+    }
+    this._page = _page
+  }
+
+  get perPage(): number {
+    return this._perPage
+  }
+
+  private set perPage(value: number) {
+    let _perPage = value === (true as any) ? this._perPage : +value
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      Number.parseInt(`${_perPage}`) !== _perPage
+    ) {
+      _perPage = 1
+    }
+    this._perPage = _perPage
+  }
+
+  get sort(): string | null {
+    return this._sort
+  }
+
+  private set sort(value: string | null) {
+    const isValid = !['', 'undefined', 'null'].includes(`${value}`)
+    this._sort = isValid ? `${value}` : null
+  }
+
+  get sortDir(): SortDirection | null {
+    return this._sortDir
+  }
+
+  private set sortDir(value: string | null) {
+    const isValid = !['', 'undefined', 'null'].includes(`${value}`)
+    if (!isValid) {
+      this._sortDir = !!this.sort ? 'ASC' : null
+      return
+    }
+    const dir = `${value}`.toUpperCase()
+    this._sortDir = ['ASC', 'DESC'].includes(dir) ? dir : ('ASC' as any)
+  }
+
+  get filter(): Filter | null {
+    return this._filter
+  }
+
+  private set filter(value: Filter | null) {
+    const isValid = !['', 'undefined', 'null'].includes(`${value}`)
+    this._filter = isValid ? (`${value}` as any) : null
+  }
+}
+
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[]
+  total: number
+  currentPage: number
+  perPage: number
+  sort: string | null
+  sortDir: string | null
+  filter: Filter | null
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[]
+  readonly total: number
+  readonly currentPage: number
+  readonly perPage: number
+  readonly lastPage: number
+  readonly sort: string | null
+  readonly sortDir: string | null
+  readonly filter: Filter
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items
+    this.total = props.total
+    this.currentPage = props.currentPage
+    this.perPage = props.perPage
+    this.lastPage = Math.ceil(this.total / this.perPage)
+    this.sort = props.sort
+    this.sortDir = props.sortDir
+    this.filter = props.filter
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    }
+  }
+}
+
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>
+> extends RepositoryInterface<E> {
+  sortableFields: string[]
+  search(props: SearchInput): Promise<SearchOutput>
+}
