a69bc9c893b8cfe1d0e1520245fbe1503250f1c3
Gibran <mac01gibran@gmail.com>
2022-04-22 20:20:56 -0300

    test: add CreateCategortUseCase

---
+++ src/category/domain/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -0,0 +1,52 @@
+import { UniqueEntityId } from './../../../../../@seedwork/domain/value-objects/unique-entity-id.vo'
+import { Category } from './../../../entities/category'
+import { CategoryInMemoryRepository } from './../../../../infra/repository/category-in-memory.repository'
+import { CreateCategortUseCase } from './../create-category.use-case'
+
+describe('CreateCategortUseCase Unit Tests', () => {
+  let createCategoryUseCase: CreateCategortUseCase
+  let categoryRepository: CategoryInMemoryRepository
+
+  beforeEach(() => {
+    categoryRepository = new CategoryInMemoryRepository()
+    createCategoryUseCase = new CreateCategortUseCase(categoryRepository)
+  })
+
+  it('should call CategoryRepository with the correct values', async () => {
+    const insertSpy = jest.spyOn(categoryRepository, 'insert')
+    const output = await createCategoryUseCase.execute({
+      name: 'test',
+      description: 'test',
+      isActive: true,
+    })
+    expect(insertSpy).toHaveBeenCalledTimes(1)
+    expect(insertSpy).toHaveBeenCalledWith(
+      new Category(
+        {
+          createdAt: output.createdAt,
+          name: 'test',
+          description: 'test',
+          isActive: true,
+        },
+        new UniqueEntityId(output.id)
+      )
+    )
+  })
+
+  it('should insert category', async () => {
+    const output = await createCategoryUseCase.execute({
+      name: 'test',
+      description: 'test',
+      isActive: true,
+    })
+    const category = await categoryRepository.findById(output.id)
+    expect(category).toBeDefined()
+    expect(output).toStrictEqual({
+      id: category.id.toString(),
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    })
+  })
+})
