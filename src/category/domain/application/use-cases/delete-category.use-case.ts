ea8ad1567e85fe31d25c4432b7045902a55de7a4
Gibran <mac01gibran@gmail.com>
2022-04-23 16:32:37 -0300

    feat: add DeleteCategoryUseCase

---
+++ src/category/domain/application/use-cases/delete-category.use-case.ts
@@ -0,0 +1,22 @@
+import { UseCase } from './../../../../@seedwork/application/use-case'
+import { CategoryRepository } from '../../repository/category.repository'
+
+export class DeleteCategoryUseCase
+  implements UseCase<DeleteCategoryUseCase.Input, DeleteCategoryUseCase.Output>
+{
+  constructor(private readonly categoryRepo: CategoryRepository.Repository) {}
+
+  async execute(
+    input: DeleteCategoryUseCase.Input
+  ): Promise<DeleteCategoryUseCase.Output> {
+    await this.categoryRepo.delete(input.id)
+  }
+}
+
+export namespace DeleteCategoryUseCase {
+  export type Input = {
+    id: string
+  }
+
+  export type Output = void
+}
