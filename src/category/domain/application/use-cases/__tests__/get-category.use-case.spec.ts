90637a042d6c7c0b1505150ccee79be5f57aeebe
Gibran <mac01gibran@gmail.com>
2022-04-23 10:17:28 -0300

    test: add GetCategoryUseCase

---
+++ src/category/domain/application/use-cases/__tests__/get-category.use-case.spec.ts
@@ -0,0 +1,37 @@
+import { Category } from './../../../entities/category'
+import { CategoryInMemoryRepository } from './../../../../infra/repository/category-in-memory.repository'
+import { GetCategoryUseCase } from './../get-category.use-case'
+
+describe('GetCategoryUseCase Unit Tests', () => {
+  let getCategoryUseCase: GetCategoryUseCase
+  let categoryRepository: CategoryInMemoryRepository
+
+  beforeEach(() => {
+    categoryRepository = new CategoryInMemoryRepository()
+    getCategoryUseCase = new GetCategoryUseCase(categoryRepository)
+  })
+
+  it('should throw when category not found', () => {
+    const promise = getCategoryUseCase.execute({
+      id: 'fake_id',
+    })
+    expect(promise).rejects.toThrowError('Entity not found')
+  })
+
+  it('should return category', async () => {
+    const findByIdSpy = jest.spyOn(categoryRepository, 'findById')
+    const category = new Category({ name: 'fake-name' })
+    await categoryRepository.insert(category)
+
+    const foundCategory = await getCategoryUseCase.execute({ id: category.id })
+
+    expect(findByIdSpy).toHaveBeenCalledWith(category.id)
+    expect(foundCategory).toStrictEqual({
+      id: category.id.toString(),
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    })
+  })
+})
